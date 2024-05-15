a3fbef6d0aa1d42928a58faeebb3e7291116af40
aleks <aprokhorenkodev@gmail.com>
2024-05-15 20:57:29 +0200

    feat: learn to how type data, course of opewebinars

---
+++ TypeScript/02-TypeData/app.js
@@ -0,0 +1,24 @@
+// ---------- Type boolean ----------
+var isLoading;
+isLoading = true;
+// ---------- Type numeric ----------
+var isNumeric;
+isNumeric = 5;
+var isNumericDecimal;
+isNumericDecimal = 5.4;
+var isNumericNegative;
+isNumericNegative = -5;
+// ---------- Type String ----------
+var isTest;
+isTest = 'example text';
+isTest = "example text 2";
+isTest = "example text 3";
+// ---------- Type Array ----------
+var isArray = [1, 'hello', true, { name: 'Aleks', age: 20 }];
+isArray.push(isLoading);
+var newArray;
+newArray.push('hello');
+// ---------- Type Any ----------
+var isAny;
+isAny = 2;
+isAny = 'spagetti';
---
+++ TypeScript/02-TypeData/app.ts
@@ -0,0 +1,26 @@
+// ---------- Type boolean ----------
+var isLoading: boolean;
+isLoading = true;
+// ---------- Type numeric ----------
+var isNumeric: number;
+isNumeric = 5;
+var isNumericDecimal: number;
+isNumericDecimal = 5.4
+var isNumericNegative: number;
+isNumericNegative = -5
+// ---------- Type String ----------
+var isTest: string;
+isTest = 'example text';
+isTest = "example text 2";
+isTest = `example text 3`;
+// ---------- Type Array ----------
+var isArray = [1, 'hello', true, {name:'Aleks', age:20}];
+isArray.push(isLoading)
+
+var newArray: string[];
+newArray.push('hello');
+
+// ---------- Type Any ----------
+var isAny: any;
+isAny = 2;
+isAny = 'spagetti';

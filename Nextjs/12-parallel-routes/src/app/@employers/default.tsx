89950c0fedb0fbdab035cc47d1a59e976907439e
Aleksander <aprokhorenkodev@gmail.com>
2025-08-03 12:50:43 +0200

    feat: create a default page to display when the user change the url

---
+++ Nextjs/12-parallel-routes/src/app/@employers/default.tsx
@@ -0,0 +1,5 @@
+const EmployerDefault = () => {
+  return <h1>Employer Default</h1>;
+};
+
+export default EmployerDefault;
---
+++ Nextjs/12-parallel-routes/src/app/@post/default.tsx
@@ -0,0 +1,5 @@
+const JobDefault = () => {
+  return <h1>Job Default</h1>;
+};
+
+export default JobDefault;

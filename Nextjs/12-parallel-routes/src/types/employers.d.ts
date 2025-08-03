f2acc23a8a7fdfff129f37f0998f29586f28bc61
Aleksander <aprokhorenkodev@gmail.com>
2025-08-03 12:49:48 +0200

    feat: add types for the actions response

---
+++ Nextjs/12-parallel-routes/src/types/employers.d.ts
@@ -0,0 +1,27 @@
+interface Employer {
+  id: number;
+  name: string;
+  username: string;
+  email: `${string}@${string}`;
+  phone: string;
+  website: string;
+  address: Address;
+  company: Company;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}

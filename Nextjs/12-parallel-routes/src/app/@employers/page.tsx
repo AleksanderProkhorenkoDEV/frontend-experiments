f5efddb8154657c6b88b58b0fc8266967b7245c3
Aleksander <aprokhorenkodev@gmail.com>
2025-08-03 12:51:21 +0200

    feat: customizate the slot

--- Nextjs/12-parallel-routes/src/app/@employers/page.tsx
+++ Nextjs/12-parallel-routes/src/app/@employers/page.tsx
@@ -1,5 +1,17 @@
-const EmployerSlot = () => {
-  return <section className="flex-1">Employeers section</section>;
+import { getEmployers } from "@/actions/employes";
+
+const EmployerSlot = async () => {
+  const employers = await getEmployers();
+  return (
+    <section className="flex-1 bg-[#63C7B2] rounded-md shadow-md h-full p-4">
+      <h1 className="text-center font-bold uppercase">Employeers section</h1>
+      <ol className="list-decimal list-inside">
+        {employers.map((employers) => {
+          return <li key={employers.id}>{employers.username}</li>;
+        })}
+      </ol>
+    </section>
+  );
 };
 
 export default EmployerSlot;

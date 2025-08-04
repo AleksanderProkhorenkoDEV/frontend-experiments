ab60b1cfa81ecc4b588eeb7bd0a40916ef94bca0
Aleksander <aprokhorenkodev@gmail.com>
2025-08-04 11:37:40 +0200

    feat: add cache and tags to optimizate the request per time

--- Nextjs/12-parallel-routes/src/actions/posts.ts
+++ Nextjs/12-parallel-routes/src/actions/posts.ts
@@ -4,7 +4,13 @@ import { Post } from "@/types/posts";
 
 export const getAllPost = async (limit?: number): Promise<Post[] | []> => {
   const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`,
+    {
+      cache: "force-cache",
+      next: {
+        tags: ["posts-collection"],
+      },
+    }
   );
   const data = await response.json();
   return data;
